1eb8ae64c0d67abbb71ee003fa7d3815046fe047
Sang Bùi <sangbui2010@gmail.com>
2018-01-12 12:08:49 +0700

    demuuu
    update

---
+++ alpr_test/alpr_test/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace openalprnet_windemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.picLicensePlate = new System.Windows.Forms.PictureBox();
+            this.picOriginal = new System.Windows.Forms.PictureBox();
+            this.lbxPlates = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picLicensePlate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnDetect.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetect.Location = new System.Drawing.Point(350, 25);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(246, 37);
+            this.btnDetect.TabIndex = 0;
+            this.btnDetect.Text = "LOAD IMAGE";
+            this.btnDetect.UseVisualStyleBackColor = false;
+            this.btnDetect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picLicensePlate
+            // 
+            this.picLicensePlate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picLicensePlate.Location = new System.Drawing.Point(12, 319);
+            this.picLicensePlate.Name = "picLicensePlate";
+            this.picLicensePlate.Size = new System.Drawing.Size(312, 90);
+            this.picLicensePlate.TabIndex = 1;
+            this.picLicensePlate.TabStop = false;
+            // 
+            // picOriginal
+            // 
+            this.picOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picOriginal.Location = new System.Drawing.Point(12, 25);
+            this.picOriginal.Name = "picOriginal";
+            this.picOriginal.Size = new System.Drawing.Size(312, 275);
+            this.picOriginal.TabIndex = 2;
+            this.picOriginal.TabStop = false;
+            // 
+            // lbxPlates
+            // 
+            this.lbxPlates.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxPlates.FormattingEnabled = true;
+            this.lbxPlates.Location = new System.Drawing.Point(350, 80);
+            this.lbxPlates.Name = "lbxPlates";
+            this.lbxPlates.Size = new System.Drawing.Size(246, 277);
+            this.lbxPlates.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Source Image:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 303);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "License Plate ROI:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(347, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Matched License Plates:";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif;" +
+    " *.png|All files (*.*)|*.*";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(350, 363);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(246, 46);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(602, 415);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbxPlates);
+            this.Controls.Add(this.picOriginal);
+            this.Controls.Add(this.picLicensePlate);
+            this.Controls.Add(this.btnDetect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NHẬN DẠNG BIỂN SỐ XE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picLicensePlate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.PictureBox picLicensePlate;
+        private System.Windows.Forms.PictureBox picOriginal;
+        private System.Windows.Forms.ListBox lbxPlates;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
